6b0c7f69dd8494be9b5517f6d75bfbe83b21ee49
programical <32033530+programical@users.noreply.github.com>
2019-10-28 20:53:04 +0100

    threading (for windows)

--- r3ts.py
+++ r3ts.py
@@ -1,22 +1,38 @@
 #!/usr/bin/python3
 
+from time import sleep
+from threading import Thread
 from appJar import gui
 import pyttsx3
 
+# change event function of text input
 def speakWord(entryId):
     text = app.getEntry(entryId)
     if " " in text:
         app.setEntry("Words", "")
-        tts.say(text)
-        tts.runAndWait()
+        Thread(target=speechThread, args=(text,)).start()
 
+# threading must be used to prevent freezing the text input
+def speechThread(word):
+    while True:
+        try:
+            sleep(0.2) # I don't know why this needs a delay, but it does bad stuff without it
+            tts.say(word)
+            tts.runAndWait()
+            break
+        except RuntimeError:
+            pass
+
+# change event function of WPM slider
 def setSpeed(scaleId):
     speed = app.getScale(scaleId)
     tts.setProperty("rate", speed)
 
+# change event function of voice select
 def setVoice(optionsId):
     tts.setProperty("voice", app.getOptionBox("Voice").split(": ")[0])
 
+# change event function of volume slider
 def setVolume(scaleId):
     tts.setProperty("volume", app.getScale("Volume")/100.0)
 
